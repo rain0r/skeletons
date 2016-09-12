4a8d672f67b17805ae826525644185ff1d047746
Rainer Hihn <rainer@hihn.org>
2016-09-12 10:04:05 +0200

    added python2/3 skeletons

---
+++ python2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: UTF-8 -*-
+
+import sys
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
